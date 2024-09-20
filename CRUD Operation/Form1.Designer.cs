8642b9fd92fb0be40bbf7c02b7a5fb2565520afc
Aijaz Ahmad <aijazahmad4276@gmail.com>
2024-09-20 14:10:55 +0530

    Add files via upload

---
+++ CRUD Operation/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace CRUD_Operation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtEmpID = new System.Windows.Forms.TextBox();
+            this.txtContact = new System.Windows.Forms.TextBox();
+            this.txtEmpName = new System.Windows.Forms.TextBox();
+            this.txtEmpAge = new System.Windows.Forms.TextBox();
+            this.cmbEmpCity = new System.Windows.Forms.ComboBox();
+            this.rdoBtnMale = new System.Windows.Forms.RadioButton();
+            this.rdoBtnFemale = new System.Windows.Forms.RadioButton();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(186, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(502, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CRUD in C# using Stored Procedure";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(69, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Employee ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(69, 329);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Gender";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(69, 275);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Employee Age";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(69, 217);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Employee City";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(69, 160);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(156, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Employee Name";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(69, 439);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 25);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Contact";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(69, 383);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(132, 25);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Joinning Date";
+            // 
+            // txtEmpID
+            // 
+            this.txtEmpID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmpID.Location = new System.Drawing.Point(313, 100);
+            this.txtEmpID.Name = "txtEmpID";
+            this.txtEmpID.Size = new System.Drawing.Size(377, 30);
+            this.txtEmpID.TabIndex = 8;
+            // 
+            // txtContact
+            // 
+            this.txtContact.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContact.Location = new System.Drawing.Point(313, 439);
+            this.txtContact.Name = "txtContact";
+            this.txtContact.Size = new System.Drawing.Size(377, 30);
+            this.txtContact.TabIndex = 11;
+            // 
+            // txtEmpName
+            // 
+            this.txtEmpName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmpName.Location = new System.Drawing.Point(313, 160);
+            this.txtEmpName.Name = "txtEmpName";
+            this.txtEmpName.Size = new System.Drawing.Size(377, 30);
+            this.txtEmpName.TabIndex = 13;
+            // 
+            // txtEmpAge
+            // 
+            this.txtEmpAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmpAge.Location = new System.Drawing.Point(313, 275);
+            this.txtEmpAge.Name = "txtEmpAge";
+            this.txtEmpAge.Size = new System.Drawing.Size(377, 30);
+            this.txtEmpAge.TabIndex = 14;
+            // 
+            // cmbEmpCity
+            // 
+            this.cmbEmpCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbEmpCity.FormattingEnabled = true;
+            this.cmbEmpCity.Items.AddRange(new object[] {
+            "None",
+            "India",
+            "Nagpur",
+            "Lucknow",
+            "U P"});
+            this.cmbEmpCity.Location = new System.Drawing.Point(313, 217);
+            this.cmbEmpCity.Name = "cmbEmpCity";
+            this.cmbEmpCity.Size = new System.Drawing.Size(377, 33);
+            this.cmbEmpCity.TabIndex = 15;
+            // 
+            // rdoBtnMale
+            // 
+            this.rdoBtnMale.AutoSize = true;
+            this.rdoBtnMale.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoBtnMale.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.rdoBtnMale.Location = new System.Drawing.Point(313, 329);
+            this.rdoBtnMale.Name = "rdoBtnMale";
+            this.rdoBtnMale.Size = new System.Drawing.Size(80, 29);
+            this.rdoBtnMale.TabIndex = 16;
+            this.rdoBtnMale.TabStop = true;
+            this.rdoBtnMale.Text = "Male";
+            this.rdoBtnMale.UseVisualStyleBackColor = true;
+            // 
+            // rdoBtnFemale
+            // 
+            this.rdoBtnFemale.AutoSize = true;
+            this.rdoBtnFemale.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoBtnFemale.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.rdoBtnFemale.Location = new System.Drawing.Point(498, 329);
+            this.rdoBtnFemale.Name = "rdoBtnFemale";
+            this.rdoBtnFemale.Size = new System.Drawing.Size(104, 29);
+            this.rdoBtnFemale.TabIndex = 17;
+            this.rdoBtnFemale.TabStop = true;
+            this.rdoBtnFemale.Text = "Female";
+            this.rdoBtnFemale.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(313, 383);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(377, 30);
+            this.dateTimePicker1.TabIndex = 18;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(179, 495);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 39);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(525, 495);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(109, 39);
+            this.btnDelete.TabIndex = 20;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.Lime;
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(347, 495);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(109, 39);
+            this.btnUpdate.TabIndex = 21;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 557);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(868, 229);
+            this.dataGridView1.TabIndex = 22;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(671, 495);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 39);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "Load Event";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(892, 798);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.rdoBtnFemale);
+            this.Controls.Add(this.rdoBtnMale);
+            this.Controls.Add(this.cmbEmpCity);
+            this.Controls.Add(this.txtEmpAge);
+            this.Controls.Add(this.txtEmpName);
+            this.Controls.Add(this.txtContact);
+            this.Controls.Add(this.txtEmpID);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "CRUD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtEmpID;
+        private System.Windows.Forms.TextBox txtContact;
+        private System.Windows.Forms.TextBox txtEmpName;
+        private System.Windows.Forms.TextBox txtEmpAge;
+        private System.Windows.Forms.ComboBox cmbEmpCity;
+        private System.Windows.Forms.RadioButton rdoBtnMale;
+        private System.Windows.Forms.RadioButton rdoBtnFemale;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ CRUD Operation/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD_Operation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-2DJ9G4B\\SQLEXPRESS02;Initial Catalog=TestSP_DB;Integrated Security=True;TrustServerCertificate=True");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int empid = int.Parse(txtEmpID.Text);
+            string empname = txtEmpName.Text, city = cmbEmpCity.Text, contact = txtContact.Text, gender = "";
+            double age = double.Parse(txtEmpAge.Text);
+            DateTime joindate = DateTime.Parse(dateTimePicker1.Text);
+            if (rdoBtnMale.Checked = true) { gender = "Male"; } else { gender = "Female"; }
+            con.Open();
+            SqlCommand cmd = new SqlCommand("exec InsertEmp_SP '"+ empid +"', '"+ empname +"', '"+ city +"', '"+ age +"', '"+ gender +"', '"+ joindate +"', '"+ contact +"'", con);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Successfully Submited.....");
+            GetEmpList();
+            con.Close();
+        }
+
+        void GetEmpList()
+        {
+            SqlCommand cmd = new SqlCommand("exec ListEmp_SP", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetEmpList();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int empid = int.Parse(txtEmpID.Text);
+            string empname = txtEmpName.Text, city = cmbEmpCity.Text, contact = txtContact.Text, gender = "";
+            double age = double.Parse(txtEmpAge.Text);
+            DateTime joindate = DateTime.Parse(dateTimePicker1.Text);
+            if (rdoBtnMale.Checked = true) { gender = "Male"; } else { gender = "Female"; }
+            con.Open();
+            SqlCommand cmd = new SqlCommand("exec UppdateEmp_SP '" + empid + "', '" + empname + "', '" + city + "', '" + age + "', '" + gender + "', '" + joindate + "', '" + contact + "'", con);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Successfully Updated.....");
+            GetEmpList();
+            con.Close();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure to delete?", "Deleted Record", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                int empid = int.Parse(txtEmpID.Text);
+                con.Open();
+                SqlCommand cmd = new SqlCommand("exec DeleteEmp_SP '" + empid + "'", con);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Successfully Deleted.....");
+                GetEmpList();
+                con.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int empid = int.Parse(txtEmpID.Text);
+            SqlCommand cmd = new SqlCommand("exec LoadEmp_SP '" + empid + "'", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
